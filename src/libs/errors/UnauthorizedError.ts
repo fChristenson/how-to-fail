ee78635fdc8f99ffd3c7bbee2d7c00b609743a94
fChristenson <fredrik.christenson@gmail.com>
2020-04-19 09:29:50 +0200

    wp

--- src/libs/errors/UnauthorizedError.ts
+++ src/libs/errors/UnauthorizedError.ts
@@ -1,6 +1,6 @@
 import { StatusError } from "./StatusError";
 
-export class NotFoundError extends StatusError {
+export class UnauthorizedError extends StatusError {
   constructor(message: string) {
     super(401, message);
   }
